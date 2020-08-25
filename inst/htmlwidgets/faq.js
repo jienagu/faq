3c643fc8b87b0aa45182d8ef53303062a1d0accb
Jiena McLellan <jienamclellan@jienas-air.attlocal.net>
2020-08-24 22:59:13 -0400

    relax labels

--- inst/htmlwidgets/faq.js
+++ inst/htmlwidgets/faq.js
@@ -34,18 +34,18 @@ faq.appendChild(faqtitle);
 
  let faqallexpand = document.createElement("button");
     faqallexpand.classList.add("faq-expand-all");
-    faqallexpand.innerHTML = "+ Expand All";
+    faqallexpand.innerHTML = x.expand_all_button_text;
 
     var allcontent = document.getElementsByClassName("faqcontent");
     var allcollapsible = document.getElementsByClassName("faqcollapsible");
     faqallexpand.addEventListener("click", function() {
-      if (faqallexpand.innerHTML === "+ Expand All") {
-              faqallexpand.innerHTML = "- Collapse All";
+      if (faqallexpand.innerHTML === x.expand_all_button_text) {
+             faqallexpand.innerHTML = x.collapse_all_button_text;
             } else {
-             faqallexpand.innerHTML = "+ Expand All";
+             faqallexpand.innerHTML = x.expand_all_button_text;
             }
     for (i = 0; i < allcontent.length; i++) {
-      if (faqallexpand.innerHTML === "+ Expand All"){
+      if (faqallexpand.innerHTML === x.expand_all_button_text){
         allcontent[i].style.maxHeight = null;
         allcollapsible[i].classList.remove("active");
       } else {
